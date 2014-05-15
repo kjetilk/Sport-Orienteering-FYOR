3e70850d52cb405d832b13039eeb62d57e9796d6
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-05-15 22:36:08 +0200

    use Moo(se) goodness

--- lib/Sport/Orienteering/FYOR.pm
+++ lib/Sport/Orienteering/FYOR.pm
@@ -10,6 +10,7 @@ use RDF::Trine qw(iri statement variable);
 use Plack::Request;
 use Scalar::Util qw(blessed);
 use RDF::Trine::Model::StatementFilter;
+use MooX::late;
 
 #use Error qw(:try);
 
@@ -58,7 +59,6 @@ sub dispatch_request {
   my ($self, $env) = @_;
   my $req = Plack::Request->new($env);
   my $uri = iri($req->uri);
-  $self->{model} = $self->model;
 
 ##############
 # First we look implement the cameras
@@ -75,16 +75,16 @@ sub dispatch_request {
 		  # }
 
 		  my $streamsmodel = RDF::Trine::Model->temporary_model;
-		  my @allstreams = $self->{model}->subjects(iri('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+		  my @allstreams = $self->model->subjects(iri('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
 																  iri('http://purl.org/dc/dcmitype/MovingImage'),
 																  undef);
 		  $streamsmodel->begin_bulk_ops();
 		  foreach my $subject (@allstreams) {
-			  my $streamsit = $self->{model}->get_statements($subject, undef, undef, undef);
+			  my $streamsit = $self->model->get_statements($subject, undef, undef, undef);
 			  while (my $sst = $streamsit->next) {
 				  $streamsmodel->add_statement( $sst );
 				  if($sst->predicate->equal(iri('http://purl.org/stuff/rev#hasReview'))) {
-					  my $votesit = $self->{model}->get_statements($sst->object, undef, undef, undef);
+					  my $votesit = $self->model->get_statements($sst->object, undef, undef, undef);
 					  while (my $vst = $votesit->next) {
 						  $streamsmodel->add_statement( $vst );
 					  }
@@ -102,7 +102,7 @@ sub dispatch_request {
 	},
 	sub (/cam/*) {
 	  sub (GET) {
-		  my $iterator = $self->{model}->get_statements(undef, undef, undef, $uri);
+		  my $iterator = $self->model->get_statements(undef, undef, undef, $uri);
 		  my ($ct, $serializer) = RDF::Trine::Serializer->negotiate($req->headers);
 		  my $output =  $serializer->serialize_iterator_to_string($iterator);
 		  return [ 200, 
@@ -114,7 +114,7 @@ sub dispatch_request {
 		  my $parser = _get_parser($req->content_type);
 		  return $parser unless (blessed($parser) && ($parser->isa("RDF::Trine::Parser"))); # Then, we didn't get a parser, but an error
 		  eval {
-			  $parser->parse_into_model($self->config->{base_uri}, $req->content, $self->{model}, context => $uri);
+			  $parser->parse_into_model($self->config->{base_uri}, $req->content, $self->model, context => $uri);
 		  }; if ($@) {
 			  return [ 400,
 						  [ 'Content-Type', 'text/plain' ],
@@ -131,7 +131,7 @@ sub dispatch_request {
 # Votes per user
   sub (/user/*/vote/*) {
 	  sub (GET) {
-		  return _just_bounded_description($self->{model}, $req);
+		  return _just_bounded_description($self->model, $req);
 	  },
 	  sub (PUT) {
 		  # TODO: Check it is the correct user
@@ -148,20 +148,20 @@ sub dispatch_request {
 						];
 		  }
 		  # Remove any previous votes
-		  $self->{model}->remove_statements($uri, iri('http://purl.org/stuff/rev#rating'), undef, undef);
+		  $self->model->remove_statements($uri, iri('http://purl.org/stuff/rev#rating'), undef, undef);
 		  # Add the statements back to the persistent model
-		  $self->{model}->begin_bulk_ops();
+		  $self->model->begin_bulk_ops();
 		  my $stream = $model->as_stream;
 		  while (my $st = $stream->next) {
-			  $self->{model}->add_statement($st);
+			  $self->model->add_statement($st);
 		  }
-		  $self->{model}->end_bulk_ops();
+		  $self->model->end_bulk_ops();
 
 		  return [ 204, [], []	];
 	  },
 	  sub (DELETE) {
-		  $self->{model}->remove_statements($uri, undef, undef, undef);
-		  $self->{model}->remove_statements(undef, undef, $uri, undef);
+		  $self->model->remove_statements($uri, undef, undef, undef);
+		  $self->model->remove_statements(undef, undef, $uri, undef);
 		  return [ 204, [], []	];
 	  }
   }
